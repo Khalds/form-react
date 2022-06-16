53ab70a3a03ed66142902caf133daf6cc3a66cdd
khald <abv.khallid@mail.ru>
2022-06-16 11:01:07 +0300

    Working in form rect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./app.css"
 
 function App() {
+  const [text, setText] = useState("")
+
+  const [textDirty, setDirty] = useState(false)
+
+  const [textMess, setMess] = useState("Поле ввода не должно быть пустой")
+
+  const [isEmpty, setIsEmpty] = useState(true)
+
+  const [butErr, setButErr] = useState(true)
+
+  //? Проверяет нажал ли я на строку инпут
+  const blurHandler = (e) => {
+    setDirty(true)
+    setMess("Поле ввода не должно быть пустой")
+    setButErr(true)
+  }
+
+  //? Действия с инпутом и проверка строки
+  const textHandler = (e) => {
+    setText(e.target.value)
+    if (e.target.value) {
+      setIsEmpty(false)
+      setMess("")
+      setButErr(false)
+    }
+    if (!e.target.value) {
+      setIsEmpty(true)
+      setMess("Поле ввода не должно быть пустой")
+      setButErr(true)
+    }
+  }
+
+  //? получение текста
+  const getText = () => {
+    setIsEmpty(true)
+    console.log(text)
+    setText("")
+    setMess("Сообщение успешно отправлено")
+    setButErr(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form onClick={(e) => e.preventDefault()}>
+        <input
+          onBlur={(e) => blurHandler(e)}
+          placeholder="Введите текст..."
+          type="text"
+          value={text}
+          onChange={(e) => textHandler(e)}
+        />
+        <button disabled={isEmpty} type="submit" onClick={(e) => getText()}>
+          отправить
+        </button>
+        {textDirty && textMess && (
+          <div className={`${butErr === true ? "is-error" : "no-error"}`}>
+            {textMess}
+          </div>
+        )}
+      </form>
+    </>
+  )
 }
 
-export default App;
+export default App
